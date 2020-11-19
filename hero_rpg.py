9a5a83243c71c934d62a43b5643d7f37111603cd
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-19 13:35:58 -0500

    Step 1

--- hero_rpg.py
+++ hero_rpg.py
@@ -6,6 +6,63 @@
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
 
+
+class Hero:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+
+
+    # def attack(self, goblin):
+    #     goblin.health -= hero.power
+    #     print("You do {} damage to the goblin.".format(hero.power))
+    #     if goblin.health <= 0:
+    #             print("The goblin is defeated.")
+
+    # def if_alive(self):
+        
+    #     while hero.health > 0:
+    #         print("You have {} health and {} power.".format(hero.health, hero.power))
+    #     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+    #     print()
+    #     print("What do you want to do?")
+    #     print("1. fight goblin")
+    #     print("2. do nothing")
+    #     print("3. flee")
+    #     print("> ", end=' ')
+    
+
+
+
+
+class Goblin:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+
+    # def if_alive(self):
+    #     while goblin.health > 0:
+    #         print("You have {} health and {} power.".format(hero.health, hero.power))
+    #     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+    #     print()
+    #     print("What do you want to do?")
+    #     print("1. fight goblin")
+    #     print("2. do nothing")
+    #     print("3. flee")
+    #     print("> ", end=' ')
+
+    # def attack(self, hero):
+    #     hero.health -= goblin.power
+    #     print("The goblin does {} damage to you.".format(goblin.power))
+    #     if hero.health <= 0:
+    #             print("You have been vanquished.")
+
+hero = Hero(10, 5)
+goblin = Goblin(6, 2)
+
+
+
+
 def main():
     hero_health = 10
     hero_power = 5
