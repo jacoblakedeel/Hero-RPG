2cd2af1ed529a43691cd83c3b3b391269a5af972
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-19 13:39:29 -0500

    Step 4

--- hero_rpg.py
+++ hero_rpg.py
@@ -19,17 +19,17 @@ class Hero:
         if goblin.health <= 0:
                 print("The goblin is defeated.")
 
-    # def if_alive(self):
+    def if_alive(self):
         
-    #     while hero.health > 0:
-    #         print("You have {} health and {} power.".format(hero.health, hero.power))
-    #     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
-    #     print()
-    #     print("What do you want to do?")
-    #     print("1. fight goblin")
-    #     print("2. do nothing")
-    #     print("3. flee")
-    #     print("> ", end=' ')
+        while hero.health > 0:
+            print("You have {} health and {} power.".format(hero.health, hero.power))
+        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        print()
+        print("What do you want to do?")
+        print("1. fight goblin")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
     
 
 hero = Hero(10, 5)
@@ -48,16 +48,16 @@ class Goblin:
         if hero.health <= 0:
                 print("You have been vanquished.")
     
-    # def if_alive(self):
-    #     while goblin.health > 0:
-    #         print("You have {} health and {} power.".format(hero.health, hero.power))
-    #     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
-    #     print()
-    #     print("What do you want to do?")
-    #     print("1. fight goblin")
-    #     print("2. do nothing")
-    #     print("3. flee")
-    #     print("> ", end=' ')
+    def if_alive(self):
+        while goblin.health > 0:
+            print("You have {} health and {} power.".format(hero.health, hero.power))
+        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        print()
+        print("What do you want to do?")
+        print("1. fight goblin")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
 
 
 
